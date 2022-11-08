e7c6a163f93476102227a5fb05fdd0cc1b614101
BKallo <Boubadjaby@hotmail.fr>
2022-11-08 07:16:43 -0600

    Get Maxand Min Valus

---
+++ Values.py
@@ -0,0 +1,18 @@
+# Challenge: Get Max and Min values 
+
+listA = [5,3,6,4]
+listB = [-3,-1,-2,-4]
+listC = [ 0,0,0,0]
+
+print (max(listA), min(listA))
+print (max(listB), min(listB))
+print (max(listC), min(listC)) 
+
+listD = []
+max = []
+min = []
+
+if max or min in listD:
+          print (max + min+ 'are in listD') 
+else:
+   print([])
